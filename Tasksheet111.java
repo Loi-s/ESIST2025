e36c63b6e921ce3ad5b754f15ef4f64d84a46781
Loi-s <robilois@gmail.com>
2024-12-07 10:51:55 +0000

    Done adding output

--- Tasksheet111.java
+++ Tasksheet111.java
@@ -3,12 +3,9 @@
  * simply prints "Hello World!" to standard output.
  */
 
-public class Tasksheet111 {
-
 class helloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
     }
 } 
 
-}
